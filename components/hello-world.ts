f76c1ab3f5021f9bd8c70cf31ba5efd323a2d051
Rupert Parry <rp@rupert.cloud>
2024-04-03 14:52:35 -0700

    Added webcharms packages

--- components/hello-world.ts
+++ components/hello-world.ts
@@ -1,16 +1,33 @@
-class HelloWorld extends HTMLElement {
-  connectedCallback() {
-    this.render();
+import { ObservableElement } from 'observable-element';
+import { Store } from 'store-thing';
+import { render, html } from 'uhtml';
+
+const store = new Store('counter', { count: 0 }, { storage: 'local' });
+
+const increment = () => store.update((state) => (state.count += 1));
+const decrement = () => store.update((state) => (state.count -= 1));
+
+class HelloWorld extends ObservableElement {
+  count: number;
+
+  constructor() {
+    super();
+    store.subscribe((state) => {
+      render(this, this.template(state.count));
+    });
   }
 
-  render() {
-    this.innerHTML = /*html*/ `
+  template(count: number) {
+    return html`
       <style>
         hello-world {
           color: blue;
         }
       </style>
       <h1>Hello world!</h1>
+      <div>Count: ${count}</div>
+      <button onclick=${increment}>+</button>
+      <button onclick=${decrement}>-</button>
     `;
   }
 }
